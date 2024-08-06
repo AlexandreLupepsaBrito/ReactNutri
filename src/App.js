84847a130278be1b426265f2467a01dc03d6f3fa
AlexandreLupepsaBrito <141967601+AlexandreLupepsaBrito@users.noreply.github.com>
2024-08-06 19:38:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'; 
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+    fetch(url)
+      .then((r) => r.json())
+      .then((json) => setNutri(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <nav className="navbar">
+        <div className="navbar-logo">React Nutri</div>
+        <ul className="navbar-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Produtos</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+      
+      <header className="promocao-header">
+        <strong className="promocao-header-title">Promoções</strong>
       </header>
+
+      <div className="row">
+        {nutri.map((item) => {
+          return (
+            <article key={item.id} className="produto-prisncipal">
+              <strong>{item.titulo}</strong>
+              <img src={item.capa} alt={item.titulo} />
+              <p>{item.subtitulo}</p>
+              <button className="buy-button">Acessar</button>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
